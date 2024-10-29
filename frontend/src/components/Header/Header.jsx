505f0b099b08513b3b7adbdbe29265c6f042e890
Tusar <tusarahmed141@gmail.com>
2024-10-29 13:35:26 +0600

    Creating different necessary pages and file

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2> Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam accusantium repellendus excepturi sunt eius voluptates dolorum perferendis autem aut aliquam assumenda ad, omnis, inventore laboriosam dolore facere quod repellat necessitatibus!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
