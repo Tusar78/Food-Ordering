41f153e244eea2b4a4ddcc1137aded9ab13f2b7b
Tusar <tusarahmed141@gmail.com>
2024-12-02 16:43:03 +0600

    Ready to admin nav and sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <hr />
+      <div className="app-component">
+        <Sidebar />
+      </div>
     </div>
   )
 }
