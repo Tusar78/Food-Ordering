8be9bebb4f400242f42a8c6a16503d5a1d398019
Tusar <tusarahmed141@gmail.com>
2024-12-03 13:33:36 +0600

    Data stay with also load

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 const LoginPopup = ({ setShowLogin }) => {
   const {url, setToken} = useContext(StoreContext);
-  const [currState, setCurrState] = useState("Sign Up");
+  const [currState, setCurrState] = useState("Login");
   const [data, setData] = useState({
     name: "",
     email: "",
